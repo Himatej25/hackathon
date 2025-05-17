e6df05366c79fc7b48294616c085f9d50b7df55c
ClytonCodes <bclytonerastus@gmail.com>
2025-05-18 03:35:35 +0530

    Update Provider.js

--- backend/models/Provider.js
+++ backend/models/Provider.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
-const ProviderSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    aadhar_card: String,
-    license: String,
-    portfolio: [String]
+const providerSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contact: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  documents: {
+    aadhar: { type: String, required: true },
+    licence: { type: String, required: true },
+    portfolio: { type: String, required: true }
+  },
+  createdAt: { type: Date, default: Date.now },
+  verified: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('Provider', ProviderSchema);
+module.exports = mongoose.model('Provider', providerSchema);
