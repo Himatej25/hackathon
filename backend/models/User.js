206b6adda653d3228f55981268d2cbb3fb49c0d1
ClytonCodes <bclytonerastus@gmail.com>
2025-05-18 14:45:18 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  contact: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['customer', 'provider'], required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
